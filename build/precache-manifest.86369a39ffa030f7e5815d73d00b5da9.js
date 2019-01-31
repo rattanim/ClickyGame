a690c55d423b03f752e15bee82389af4062e8044
Mirage Rattani <m.rattani0905@gmail.com>
2019-01-31 11:21:11 -0500

    forcing commit

---
+++ build/precache-manifest.86369a39ffa030f7e5815d73d00b5da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8135d3b64ba6f096b9d3",
+    "url": "/static/css/main.c284b3f6.chunk.css"
+  },
+  {
+    "revision": "8135d3b64ba6f096b9d3",
+    "url": "/static/js/main.8135d3b6.chunk.js"
+  },
+  {
+    "revision": "441b227c7c9e041cd9b4",
+    "url": "/static/js/1.441b227c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "995cc0e8a2cfe29ef21facea5396934a",
+    "url": "/index.html"
+  }
+];
