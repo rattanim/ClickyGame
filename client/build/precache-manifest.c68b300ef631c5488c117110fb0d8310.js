c414c6fc32a123de11e78961a67b6bb638019145
Mirage Rattani <m.rattani0905@gmail.com>
2019-01-31 11:04:32 -0500

    pushing everything

---
+++ client/build/precache-manifest.c68b300ef631c5488c117110fb0d8310.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c6b7d1e818f833d644a",
+    "url": "/static/css/main.15ed6e3a.chunk.css"
+  },
+  {
+    "revision": "4c6b7d1e818f833d644a",
+    "url": "/static/js/main.4c6b7d1e.chunk.js"
+  },
+  {
+    "revision": "441b227c7c9e041cd9b4",
+    "url": "/static/js/1.441b227c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e685dbcc9c6bc506889b7439d8a6cfb6",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>Click Game!</h1>
+			<h2> Click on a picture to get points! But if you click on the same image twice the game restarts!!</h2>
+		</header>
+	);
+}
+
+export default Header;
